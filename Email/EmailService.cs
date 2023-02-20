3e1e2a06b15a91abfc36121bb4cdc97fcdcd434d
Mantas Dirginčius <mantas.dirgincius@gmail.com>
2023-02-20 12:27:46 +0200

    .net7 migration

--- Email/EmailService.cs
+++ Email/EmailService.cs
@@ -17,14 +17,14 @@ namespace Email
         public void SendMail(EmailDto request)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("userhubservice@gmail.com"));
+            email.From.Add(MailboxAddress.Parse(_email.GetSection("EmailUsername").Value));
             email.To.Add(MailboxAddress.Parse(request.To));
             email.Subject = request.Subject;
             email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
 
             using var smtp = new SmtpClient();
-            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("userhubservice@gmail.com", "Testas123");
+            smtp.Connect(_email.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_email.GetSection("EmailUsername").Value, _email.GetSection("EmailPassword").Value);
             smtp.Send(email);
             smtp.Disconnect(true);
         }
